193cc96a067830f70672f8b73956203f2ec42e88
Eduard <edwardbs876@gmail.com>
2023-03-28 20:45:29 +0300

    Bean validation implementation

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @Deprecated
@@ -27,7 +28,7 @@ public class BeerController {
     }
 
     @PostMapping // CREATE NEW BEER
-    public ResponseEntity handlePost(@RequestBody BeerDto beerDto) {
+    public ResponseEntity handlePost(@Valid @RequestBody BeerDto beerDto) {
 
         BeerDto savedDto = beerService.saveNewBeer(beerDto);
 
@@ -38,7 +39,7 @@ public class BeerController {
     }
 
     @PutMapping("{beerId}")
-    public ResponseEntity handlePut(@PathVariable UUID beerId ,@RequestBody BeerDto beerDto) {
+    public ResponseEntity handlePut(@PathVariable UUID beerId ,@Valid @RequestBody BeerDto beerDto) {
 
         beerService.updateBeer(beerId, beerDto);
 
